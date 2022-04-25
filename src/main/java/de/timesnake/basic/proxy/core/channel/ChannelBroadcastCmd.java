d028ad64cab91c8b4a8c79900acf2d1cbe379f9a
funzter <funzter@mail.timesnake.de>
2022-04-25 11:57:11 +0200

    cleanup

--- src/main/java/de/timesnake/basic/proxy/core/channel/ChannelBroadcastCmd.java
+++ src/main/java/de/timesnake/basic/proxy/core/channel/ChannelBroadcastCmd.java
@@ -1,9 +1,9 @@
 package de.timesnake.basic.proxy.core.channel;
 
+import de.timesnake.basic.proxy.util.Network;
 import de.timesnake.basic.proxy.util.chat.Argument;
 import de.timesnake.basic.proxy.util.chat.ChatColor;
 import de.timesnake.basic.proxy.util.chat.Sender;
-import de.timesnake.channel.core.ChannelInfo;
 import de.timesnake.library.extension.util.cmd.Arguments;
 import de.timesnake.library.extension.util.cmd.CommandListener;
 import de.timesnake.library.extension.util.cmd.ExCommand;
@@ -15,8 +15,8 @@ public class ChannelBroadcastCmd implements CommandListener<Sender, Argument> {
     @Override
     public void onCommand(Sender sender, ExCommand<Sender, Argument> cmd, Arguments<Argument> args) {
         if (sender.isConsole(true)) {
-            ChannelInfo.setBroadcast(!ChannelInfo.getBroadcast());
-            sender.sendPluginMessage(ChatColor.PERSONAL + "Broadcast channel-messages: " + ChannelInfo.getBroadcast());
+            Network.getChannel().printInfoLog(!Network.getChannel().isPrintingInfoLog());
+            sender.sendPluginMessage(ChatColor.PERSONAL + "Broadcast channel-messages: " + Network.getChannel().isPrintingInfoLog());
         }
     }
 

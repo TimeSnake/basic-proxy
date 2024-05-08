a558f0fd368fa0bdcf019653900d07f4b958af45
funzter <cavehive@protonmail.com>
2024-05-09 01:20:03 +0200

    fix logger cmd

--- src/main/java/de/timesnake/basic/proxy/core/cmd/LoggerCmd.java
+++ src/main/java/de/timesnake/basic/proxy/core/cmd/LoggerCmd.java
@@ -8,14 +8,13 @@ import de.timesnake.basic.proxy.util.chat.Argument;
 import de.timesnake.basic.proxy.util.chat.CommandListener;
 import de.timesnake.basic.proxy.util.chat.Completion;
 import de.timesnake.basic.proxy.util.chat.Sender;
-import de.timesnake.library.basic.util.LogHelper;
 import de.timesnake.library.commands.PluginCommand;
 import de.timesnake.library.commands.simple.Arguments;
 import de.timesnake.library.permissions.Permission;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.util.List;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 
 public class LoggerCmd implements CommandListener {
@@ -26,31 +25,23 @@ public class LoggerCmd implements CommandListener {
     args.isLengthEqualsElseExit(2, true);
 
     String loggerName = args.getString(0);
-    Logger logger = LogHelper.LOGGER_BY_NAME.get(loggerName);
-
-    if (logger == null) {
-      sender.sendPluginTDMessage("§wLogger §v" + loggerName + "§w not found");
-      return;
-    }
-
     String levelName = args.getString(1).toUpperCase();
 
-    Level level;
+    org.apache.logging.log4j.Level level;
     try {
-      level = Level.parse(levelName);
+      level = org.apache.logging.log4j.Level.getLevel(levelName);
     } catch (IllegalArgumentException e) {
       sender.sendPluginTDMessage("§wUnable to parse log-level §v" + loggerName);
       return;
     }
 
-    logger.setLevel(level);
-    logger.setUseParentHandlers(level == Level.INFO);
+    Configurator.setAllLevels(loggerName, level);
     sender.sendPluginTDMessage("Updated log-level of §v" + loggerName + "§s to §v" + levelName);
   }
 
   @Override
   public Completion getTabCompletion() {
-    return new Completion((sender, cmd, args) -> sender.isConsole(false) ? LogHelper.LOGGER_BY_NAME.keySet() : List.of())
+    return new Completion((sender, cmd, args) -> sender.isConsole(false) ? List.of("<name>") : List.of())
         .addArgument(new Completion(Stream.of(Level.OFF, Level.SEVERE, Level.WARNING, Level.INFO, Level.ALL)
             .map(Level::getName).toList()));
   }
